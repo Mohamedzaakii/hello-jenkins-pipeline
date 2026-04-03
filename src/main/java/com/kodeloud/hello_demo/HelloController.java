3fa540be37aa744d408427f4b1d2bfc41aae1ade
Mohamedzaakii <mohamedzaki827@gmail.com>
2026-04-03 09:20:37 +0200

    Fix: Separate HelloDemoApplication and HelloController into correct files

--- src/main/java/com/kodeloud/hello_demo/HelloController.java
+++ src/main/java/com/kodeloud/hello_demo/HelloController.java
@@ -1,11 +1,13 @@
 package com.kodeloud.hello_demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class HelloDemoApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(HelloDemoApplication.class, args);
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/hello")
+    String hello() {
+        return "Hello, KodeKloud community!";
     }
 }
